8f7e3388a7673d91cd84fa129410399c62a5b8e9
Jason Lane <quizathaderat@yahoo.com>
2018-10-09 14:22:15 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import './App.css';
+import logo from './logo.svg';
+import BarChart from "./components/bar_chart"
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <BarChart data={[1, 2, 3, 4, 5]} height={500} width={500} />
+      </div>
+    );
+  }
+}
+
+export default App;
